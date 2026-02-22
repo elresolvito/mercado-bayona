9ff6e2cb3127ab35bb9efc4355a6104c2015af5f
jccarlos010277-cmd <jccarlos010277@gmail.com>
2026-02-22 13:43:04 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:base: '/',  // ← CAMBIADO AQUÍ
+   base: '/',  // ← CAMBIADO AQUÍ
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
