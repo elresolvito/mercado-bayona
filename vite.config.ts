4bac336b5e0fbeffa7b77512812322fd81e3e686
jccarlos010277-cmd <jccarlos010277@gmail.com>
2026-02-22 13:22:55 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mercado-bayona/',  // ← CAMBIADO AQUÍ
+  base:base: '/',  // ← CAMBIADO AQUÍ
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
